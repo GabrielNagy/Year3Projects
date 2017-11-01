75dc553d7979aa7acc146f7868dc590d57aedc64
Gabriel Nagy <gabrielnagy@me.com>
2017-11-01 16:11:05 +0200

    [WT] Add lab 3 and 4

---
+++ WT/Lab4/flaskTemplate/run.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import xmltodict
+import json
+app = Flask(__name__)
+
+
+@app.route("/")
+def template_test():
+    with open('queue.xml') as fd:
+        doc = xmltodict.parse(fd.read())
+    doc = json.dumps(doc['queue']['link'])
+    xml_json = json.loads(doc)
+    return render_template('template.html', my_string="Wheeeee!", my_list=[0, 1, 2, 3, 4, 5], xml_json=xml_json)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
