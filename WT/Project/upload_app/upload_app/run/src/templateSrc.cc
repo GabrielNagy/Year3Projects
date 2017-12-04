4a69b0d5ed7da35fd6788897030161395e5c3929
Gabriel Nagy <gabriel.nagy@nokia.com>
2017-12-04 12:43:20 +0200

    [WT] Randomize make check duration

--- WT/Project/upload_app/upload_app/run/src/templateSrc.cc
+++ WT/Project/upload_app/upload_app/run/src/templateSrc.cc
@@ -1,7 +1,13 @@
 #include "templateSrc.h"
+#include <random>
+#include <unistd.h>
 
 Stub::Stub()
 {
+    std::random_device rd;
+    std::mt19937 eng(rd());
+    std::uniform_int_distribution<> distr(100000, 1000000);
+    usleep(distr(eng));
 }
 
 
