ed39ace7c038e93ccafb763e6653c9ef0cc7c0dc
Gabriel Nagy <gabrielnagy@me.com>
2017-12-07 19:11:58 +0200

    [WT] Fix map generation

--- WT/Project/upload_app/upload_app/_design/users/views/by_total/map.js
+++ WT/Project/upload_app/upload_app/_design/users/views/by_total/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-  if (doc.doc_type == "Entry") {
+  if (doc.doc_type == "Entry" && doc.points && doc.total) {
   emit([doc.grade, doc.author, doc.problem, doc.points, doc.total, doc.language], [doc.points, doc.total]);
  }
 }
