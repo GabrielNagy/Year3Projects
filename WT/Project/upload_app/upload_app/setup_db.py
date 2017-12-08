e87f5cbfa9d56f2bb872e43ea6e9da21d2d1b5f0
Gabriel Nagy <gabrielnagy@me.com>
2017-12-08 09:29:07 +0200

    [WT] Correctly push design documents regardless of the working folder

--- WT/Project/upload_app/upload_app/setup_db.py
+++ WT/Project/upload_app/upload_app/setup_db.py
@@ -4,6 +4,7 @@ import os
 
 
 if __name__ == "__main__":
+    path = os.path.dirname(os.path.abspath(__file__))
     couchdbkit.set_logging('info')
     if 'COUCHDB_USER' and 'COUCHDB_PASS' in os.environ:
         ip = "http://%s:%s@127.0.0.1:5984" % (os.getenv('COUCHDB_USER'), os.getenv('COUCHDB_PASS'))
@@ -12,4 +13,4 @@ if __name__ == "__main__":
     database = "upload_app"
     server = couchdbkit.Server(ip)
     db = server.get_or_create_db(database)
-    couchdbkit.designer.push('_design/users', db)
+    couchdbkit.designer.push(os.path.join(path, '_design', 'users'), db)
