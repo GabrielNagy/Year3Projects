ba80772c8a281c28f6ea9df56a924ce56bd5eda8
Gabriel Nagy <gabriel.nagy@nokia.com>
2018-01-08 11:24:59 +0200

    [DP] Further additions

--- DP/src/ro/uvt/dp/Client.java
+++ DP/src/ro/uvt/dp/Client.java
@@ -3,7 +3,7 @@ package ro.uvt.dp;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import ro.uvt.dp.Account.TYPE;
+import ro.uvt.dp.TYPE;
 
 /**
  * 
@@ -18,18 +18,39 @@ public class Client {
 	private ArrayList<Account> accounts = new ArrayList<>();
 	private String cnp = null;
 
-	public Client(String nume, String adresa, TYPE tip, String numarCont, double suma, String cnp) {
+	private Client(String nume, String adresa, TYPE tip, String numarCont, double suma, String cnp) {
 		this.name = nume;
 		this.address = adresa;
 		this.cnp = cnp;
 		addAccount(tip, numarCont, suma);
 	}
 
+  public static class ClientBuilder {
+    private final String name;
+    private final String address;
+    private final Account accounts[];
+    private String dateOfBirth;
+
+    public ClientBuilder(String name, String address, Account accounts[]) {
+      this.name = name;
+      this.address = address;
+      this.accounts = new Account[NUMAR_MAX_CONTURI];
+    }
+
+    public ClientBuilder dob(String dateOfBirth) {
+      this.dateOfBirth = dateOfBirth;
+      return this;
+    }
+
+    public Client build() {
+      return new Client(this);
+    }
+  }
 	public boolean addAccount(TYPE tip, String accountCode, double suma) {
 		Account c = null;
-		if (tip == Account.TYPE.EUR)
+		if (tip == TYPE.EUR)
 			c = new AccountEUR(accountCode, suma);
-		else if (tip == Account.TYPE.RON)
+		else if (tip == TYPE.RON)
 			c = new AccountRON(accountCode, suma);
 		if(accounts.size() + 1 <= MAX_ACCOUNT_NUMBER) {
 			accounts.add(c);
