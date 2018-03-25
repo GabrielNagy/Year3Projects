5f5f6742a309083a5aec4f03f352d13933353785
Gabriel Nagy <gabrielnagy@me.com>
2018-03-25 17:31:50 +0300

    [NET] Add Homework 2

---
+++ NET/Hw2/Client.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Connect(String server, String message)
+        {
+            try
+            {
+                Int32 port = 12345;
+                TcpClient client = new TcpClient(server, port);
+
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                NetworkStream stream = client.GetStream();
+
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine("Client Sent: {0}", message);
+
+                data = new Byte[256];
+                String responseData = String.Empty;
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Client Received: {0}", responseData);
+
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e) { Console.WriteLine("ArgumentNullException: {0}", e); }
+            catch (SocketException e) { Console.WriteLine("SocketException: {0}", e); }
+            Console.WriteLine("\n Press enter to continue...");
+            Console.Read();
+        }
+        static void Main(string[] args)
+        {
+            Connect("127.0.0.1", @"C:\a");
+        }
+    }
+}
---
+++ NET/Hw2/Listener.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listener
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {
+                Int32 port = 12345;
+                IPAddress localAddress = IPAddress.Parse("127.0.0.1");
+
+                server = new TcpListener(localAddress, port);
+
+                server.Start();
+
+                Byte[] bytes = new Byte[256];
+                String data = null;
+
+                while (true)
+                {
+                    Console.Write("Waiting for a connection...");
+
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    data = null;
+
+                    NetworkStream stream = client.GetStream();
+
+                    int i;
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Server Received: {0}", data);
+                        string text = data;
+                        if (System.IO.File.Exists(data))
+                        {
+                            text = System.IO.File.ReadAllText(data);
+                        }
+                        else if (System.IO.Directory.Exists(data))
+                        {
+                            text = string.Join("\n", System.IO.Directory.GetFiles(data));
+                        } else { text = "Path does not exist."; }
+
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(text);
+                        stream.Write(msg, 0, msg.Length);
+                        Console.WriteLine("Server Sent: {0}", text);
+                    }
+
+                    client.Close();
+                }
+            }
+            catch (SocketException e) { Console.WriteLine("SocketException: {0}", e); }
+            finally { server.Stop(); }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+    }
+
+}
